9974bc04e4f8cfe83a6f08a4aa8938033ee20891
Cristian Neftali Hernandez Ramirez <cristian.hernandez@therocketcode.com>
2025-12-09 22:30:00 -0600

    ✨ Configure URL routing for perfumes app

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/accounts/home/', permanent=False)),
+    path('', RedirectView.as_view(url='/perfumes/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('perfumes/', include('perfumes.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
