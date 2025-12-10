2f96ca0488384b3763bad3ff3328529d2e9952ac
Cristian Neftali Hernandez Ramirez <cristian.hernandez@therocketcode.com>
2025-12-09 20:57:41 -0600

    ✨ Configure admin interfaces for perfume

--- rubiPerfumeria/admin.py
+++ rubiPerfumeria/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Perfume
 
-# Register your models here.
+@admin.register(Perfume)
+class PerfumeAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'marca', 'tipo', 'genero', 'volumen', 'precio', 'stock', 'activo', 'fecha_creacion']
+    list_filter = ['tipo', 'genero', 'activo', 'marca']
+    search_fields = ['nombre', 'marca', 'descripcion']
+    list_editable = ['stock', 'activo']
+    readonly_fields = ['fecha_creacion', 'fecha_actualizacion']
+    fieldsets = (
+        ('Información Básica', {
+            'fields': ('nombre', 'marca', 'descripcion', 'imagen')
+        }),
+        ('Características', {
+            'fields': ('tipo', 'genero', 'volumen', 'precio')
+        }),
+        ('Inventario', {
+            'fields': ('stock', 'activo')
+        }),
+        ('Fechas', {
+            'fields': ('fecha_creacion', 'fecha_actualizacion'),
+            'classes': ('collapse',)
+        }),
+    )
