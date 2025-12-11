9e22d7a87e0f8d1cfd6f21848d368f878926c1b4
Cristian Neftali Hernandez Ramirez <cristian.hernandez@therocketcode.com>
2025-12-11 16:00:00 -0600

    ✨ Update login redirections based on user roles

--- perfumes/views.py
+++ perfumes/views.py
@@ -6,13 +6,13 @@ from .models import Perfume
 from .forms import PerfumeForm
 
 
-@login_required
+@supervisor_required
 def perfume_list(request):
     perfumes = Perfume.objects.all()
     return render(request, 'perfumes/perfume_list.html', {'perfumes': perfumes})
 
 
-@login_required
+@supervisor_required
 def perfume_detail(request, pk):
     perfume = get_object_or_404(Perfume, pk=pk)
     return render(request, 'perfumes/perfume_detail.html', {'perfume': perfume})
